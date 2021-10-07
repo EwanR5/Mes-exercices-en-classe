c665cae9c33f22fd459379ce2feec127124c3a89
EwanR5 <91669749+EwanR5@users.noreply.github.com>
2021-10-07 14:32:18 +0200

    Add files via upload

---
+++ AlgoAge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace AlgoAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string categorie = "";
+            int age = 0;
+            Console.WriteLine("Hello World!");
+            categorieAge(age, ref categorie);
+        }
+
+        public static void categorieAge(int age, ref string categorie)
+        {
+            if (age < 6)
+            {
+                categorie = "Vous êtes trop jeune";
+            }
+            else if (age <= 7)
+            {
+                categorie = "Vous êtes Poussin";
+            }
+            else if (age <= 9)
+            {
+                categorie = "Vous êtes Pupille";
+            }
+            else if (age <= 11)
+            {
+                categorie = "Vous êtes Minime";
+            }
+            else if (age <= 14)
+            {
+                categorie = "Vous êtes Cadet";
+            }
+            else
+            {
+                categorie = "Vous êtes trop âgé";
+            }
+
+              
+        }
+    }
+}
---
+++ AlgoAge/categorie.cs
@@ -0,0 +1,6 @@
+﻿namespace AlgoAge
+{
+    public class categorie
+    {
+    }
+}
